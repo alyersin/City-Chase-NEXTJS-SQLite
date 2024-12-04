a7c30f0210118d1d804ad13a48a76d44ab6d8ed7
alyersin <alyerisn@yahoo.com>
2024-12-04 13:40:52 +0200

    04-12 13:40

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Expose environment variables for client-side use (safe variables only)
-  env: {
-    AMADEUS_API_KEY: process.env.AMADEUS_API_KEY, // Safe to expose if needed
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
--- src/app/pages/cityDetails/CityDetails.js
+++ src/app/pages/cityDetails/CityDetails.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from "react";
 import { Box, Heading, Text, Button } from "@chakra-ui/react";
 
